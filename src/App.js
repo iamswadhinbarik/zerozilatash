8c71d913428079d9246fa5923e14a2657cd2a016
swadhin <swadhinbanty143@gmail.com>
2021-12-06 13:39:35 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
+import { Home } from "./pages/home";
+import { Products } from "./pages/products";
+import { ProductDetails } from "./pages/product";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/home" element={<Home />} />
+        <Route exact path="/products/:catName" element={<Products />} />
+        <Route
+          exact
+          path="/product-details/:productId"
+          element={<ProductDetails />}
+        />
+      </Routes>
+    </Router>
   );
 }
 
